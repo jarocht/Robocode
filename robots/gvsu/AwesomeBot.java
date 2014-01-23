50a61a77498176561d865e683f0e021842278065
octagoncow <hutekt@mail.gvsu.edu>
2014-01-22 20:33:56 -0500

    Fixed fire detection
    Fire detection was fixed and detaching the radar from the gun and robot
    was implemented. I might work on this later.

--- robots/gvsu/AwesomeBot.java
+++ robots/gvsu/AwesomeBot.java
@@ -1,5 +1,6 @@
 package gvsu;
 import robocode.*;
+import java.awt.Color;
 import static robocode.util.Utils.normalRelativeAngleDegrees;
 //import java.awt.Color;
 
@@ -10,18 +11,23 @@ import static robocode.util.Utils.normalRelativeAngleDegrees;
  */
 public class AwesomeBot extends Robot
 {
+	Radar radar = new Radar();
+	public double count =0; //for testing how often enemy fires
 	private double eEnergy;
 	private boolean move;
 	/**
 	 * run: AwesomeBot's default behavior
 	 */
 	public void run() {
+		//detaches radar from robot and gun movement
+		radar.detach();
+
 		// Initialization of the robot should be put here
 		eEnergy = 0;
 		// After trying out your robot, try uncommenting the import at the top,
 		// and the next line:
 
-		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
+		 setColors(Color.pink,Color.pink,Color.pink); // body,gun,radar
 
 		// Robot main loop
 		while(true) {
@@ -43,24 +49,28 @@ public class AwesomeBot extends Robot
 	public void onScannedRobot(ScannedRobotEvent e) {
 		if (eEnergy == 0){
 			eEnergy = e.getEnergy();
-		} else if (eEnergy - e.getEnergy() < 3){
+			//each bullet fired (with walls) uses 2 energy
+		} else if (eEnergy - e.getEnergy() == 2){
+			count++;
+			System.out.println(count);
 			System.out.println("shot fired!");
 			move = true;
 		} eEnergy = e.getEnergy();	
 		
 		// Calculate exact location of the robot
 		double absoluteBearing = getHeading() + e.getBearing();
+		System.out.println(absoluteBearing);
 		//calculate next likely corner here
 		double bearingFromRadar = normalRelativeAngleDegrees(absoluteBearing - getRadarHeading());
 
 		// If it's close enough, fire!
-		if (Math.abs(bearingFromRadar) <= 3) {
+		if (Math.abs(bearingFromRadar) <= 20) {
 			turnRadarRight(bearingFromRadar);
 			// We check gun heat here, because calling fire()
 			// uses a turn, which could cause us to lose track
 			// of the other robot.
 			if (getGunHeat() == 0) {
-				//fire(Math.min(3 - Math.abs(bearingFromRadar), getEnergy() - .1));
+				fire(Math.min(3 - Math.abs(bearingFromRadar), getEnergy() - .1));
 			}
 		} // otherwise just set the gun to turn.
 		// Note:  This will have no effect until we call scan()
@@ -89,6 +99,20 @@ public class AwesomeBot extends Robot
 	 */
 	public void onHitWall(HitWallEvent e) {
 		// Replace the next line with any behavior you would like
-		back(20);
+		//back(20);
 	}	
+
+
+	class Radar{
+		public void detach(){
+			//Radar doesn't turn with robot
+			setAdjustRadarForRobotTurn(true);
+			//Radar doesn't turn with gun
+			setAdjustRadarForGunTurn(true);
+		}
+		
+	
+	}
 }
+
+
