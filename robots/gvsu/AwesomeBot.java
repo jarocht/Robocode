9a39b552e39f919bdb167a6ce0d1f41d99f98e95
octagoncow <hutekt@mail.gvsu.edu>
2014-01-24 23:17:52 -0500

    Implemented predictive firing
    Implemented predictive firing and enemy coordinate tracking.

--- robots/gvsu/AwesomeBot.java
+++ robots/gvsu/AwesomeBot.java
@@ -20,6 +20,9 @@ public class AwesomeBot extends AdvancedRobot
 	 * run: AwesomeBot's default behavior
 	 */
 	public void run() {
+	
+		//allows the robot to wait till it's at the center before using its radar
+		//boolean center = false;
 		//detaches radar from robot and gun movement
 		detach(true);
 		// Initialization of the robot should be put here
@@ -60,7 +63,7 @@ public class AwesomeBot extends AdvancedRobot
 		if (eEnergy < 3){
 				double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
 				turnGunRight(bearingFromGun);
-				fire(2);
+				
 		}
 
 		// Calculate exact location of the robot
@@ -68,12 +71,14 @@ public class AwesomeBot extends AdvancedRobot
 		turnRadarRight(bearingFromRadar);
 				
 		//Turn gun to that corner
+		
 		double cornerHeading = nextCornerHeading(e); //Trig HERE
+
 		double bearingFromGun = cornerHeading - getGunHeading();
 		if (bearingFromGun != 0)
 			turnGunRight(bearingFromGun);
 		else
-			fire(2);
+			//fire(2);
 
 		if (bearingFromRadar == 0) {
 			scan();
@@ -110,21 +115,123 @@ public class AwesomeBot extends AdvancedRobot
 		switch((int)e.getHeading()){
 			case 0:
 			case 360:
-				System.out.println("Top Left");
+				//System.out.println("Top Left");
+
+				getCoordinates("left", e);
 				return Math.toDegrees(Math.atan( (getBattleFieldHeight()-getY()) / (getX()-0.0))) + 270.0;
+
 			case 90:
-				System.out.println("Top Right");
+				//System.out.println("Top Right");
+
+				getCoordinates("top", e);
 				return 90 - Math.toDegrees(Math.atan( (getBattleFieldHeight()-getY()) / (getBattleFieldWidth()-getX())));
+
 			case 180:
-				System.out.println("Bottom Right");
+				//System.out.println("Bottom Right");
+
+				getCoordinates("right", e);
 				return Math.toDegrees(Math.atan( (getY()-0.0) / (getBattleFieldWidth()-getX()))) + 90.0;
+
 			case 270:
-				System.out.println("Bottom Left");
+				//System.out.println("Bottom Left");
+
+				getCoordinates("bottom", e);
 				return 270 - Math.toDegrees(Math.atan( (getY()-0.0) / (getX()-0.0)));
+
 			default:
 				System.out.println("In Transition!");
 				return getGunHeading();
 		}	
+
+	}
+	
+	public void getCoordinates(String side, ScannedRobotEvent e){
+		double hypot = e.getDistance();
+
+		double X = 0;
+		double Y = 0;
+		double adjacent;
+		double opposite;
+		double enemyVelocity = e.getVelocity();
+		double enemyTime;
+		double cornerDistance;
+		double fireTime;
+		double bulletVelocity;
+		double bulletPower = 3;
+		//System.out.println(angle);
+		double angle = (getRadarHeading()); //should give the angle of the triangle in degrees
+		if(side.equals("top")){
+			Y =getBattleFieldHeight()-18;
+			adjacent = Y-getY(); //getting the adjacent side length by measuring distance from top wall
+			opposite = Math.sqrt(Math.pow(hypot,2)-Math.pow(adjacent,2));
+			if(angle>270){ //for when the enemy is to the left of you.
+				opposite*=-1;
+			}
+			X = opposite + getX()+16;
+			//System.out.println(X + "," + Y);
+			enemyTime = (800-X)/enemyVelocity;
+			cornerDistance = Math.sqrt(Math.pow((getX()-800),2)+Math.pow((getY()-600),2)); //corner is 0,0
+		}
+
+		else if(side.equals("bottom")){
+			Y =18;
+			adjacent = Y-getY(); //getting the adjacent side length by measuring distance from top wall
+			opposite = Math.sqrt(Math.pow(hypot,2)-Math.pow(adjacent,2));
+			if(angle>180){ //for when the enemy is to the left of you.
+				opposite*=-1;
+			}
+			X = opposite + getX()-16;
+			//System.out.println(X + "," + Y);
+			enemyTime = X/enemyVelocity;
+			cornerDistance = Math.sqrt(Math.pow(getX(),2)+Math.pow(getY(),2)); //corner is 0,0
+		}
+		
+		else if(side.equals("left")){
+			X =18;
+			adjacent = X-getX(); //getting the adjacent side length by measuring distance from top wall
+
+			opposite = Math.sqrt(Math.pow(hypot,2)-Math.pow(adjacent,2));
+			if(angle<270){ //for when the enemy is to the left of you.
+				opposite*=-1;
+			}
+			Y = opposite + getY()+16;
+			//System.out.println(X + "," + Y);
+			enemyTime = (600-Y)/enemyVelocity;
+			cornerDistance = Math.sqrt(Math.pow(getX(),2)+Math.pow((getY()-600),2)); //corner is 0,600
+		}
+		
+	 else if(side.equals("right")){
+			X =getBattleFieldWidth()-18;
+			adjacent = X-getX(); //getting the adjacent side length by measuring distance from top wall
+
+			opposite = Math.sqrt(Math.pow(hypot,2)-Math.pow(adjacent,2));
+			if(angle>90){ //for when the enemy is to the left of you.
+				opposite*=-1;
+			}
+			Y = opposite + getY()-16; // 16 subtracted for gun 
+			//System.out.println(X + "," + Y);
+			enemyTime = Y/enemyVelocity;
+			cornerDistance = Math.sqrt(Math.pow((getX()-800),2)+Math.pow(getY(),2)); //corner is 800,0
+		}
+	else{
+		cornerDistance = 900;
+		enemyTime = 999999;
+	}
+
+		//coordinates are calculated at this point
+		bulletVelocity = 20-3*bulletPower;
+		fireTime = cornerDistance/bulletVelocity;
+		System.out.println("Bullet Time: "+fireTime + "enemy Time: " + enemyTime);
+		double fireRatio = fireTime/enemyTime;
+		
+		double tolerance = .02;
+		if(fireRatio >(1-tolerance) && fireRatio<(1+tolerance)){
+		System.out.println("ratio: " + fireRatio);
+		System.out.println("FIRE!");
+		fire(bulletPower);
+		}
+		 
+	
 
 	}
 }
